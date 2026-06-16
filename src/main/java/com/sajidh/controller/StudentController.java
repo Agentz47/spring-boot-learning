edcd0763d3322286f32b89bb586740dab8e18bc2
Sajidh Yazeen <155213605+Agentz47@users.noreply.github.com>
2026-06-16 20:12:24 +0530

    getAll method added in StudentController

--- src/main/java/com/sajidh/controller/StudentController.java
+++ src/main/java/com/sajidh/controller/StudentController.java
@@ -1,4 +1,24 @@
 package com.sajidh.controller;
 
+import com.sajidh.model.Student;
+import com.sajidh.service.StudentService;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
 public class StudentController {
+
+    private final StudentService service;
+
+    public StudentController(
+            StudentService service
+    ) {
+        this.service = service;
+    }
+
+    @GetMapping("/students")
+    public List<Student> getAllStudents() {
+
+        return service.getAllStudents();
+    }
 }
