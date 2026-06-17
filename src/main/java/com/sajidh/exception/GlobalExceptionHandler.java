a13e3a52e186892abb3dd374a961a0260a51c03b
Sajidh Yazeen <155213605+Agentz47@users.noreply.github.com>
2026-06-17 20:00:17 +0530

    Exception handling was added with custom error messages

---
+++ src/main/java/com/sajidh/exception/GlobalExceptionHandler.java
@@ -0,0 +1,42 @@
+package com.sajidh.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(
+            MethodArgumentNotValidException.class
+    )
+    @ResponseStatus(
+            HttpStatus.BAD_REQUEST
+    )
+    public ErrorResponse handleValidationErrors(
+            MethodArgumentNotValidException ex
+    ) {
+
+        List<String> errors =
+                new ArrayList<>();
+
+        ex.getBindingResult()
+                .getFieldErrors()
+                .forEach(error ->
+                        errors.add(
+                                error.getDefaultMessage()
+                        )
+                );
+
+        return new ErrorResponse(
+                "Validation Failed",
+                errors
+        ) {
+        };
+    }
+}
