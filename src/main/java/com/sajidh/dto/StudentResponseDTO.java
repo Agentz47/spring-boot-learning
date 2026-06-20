444b3528167464ef16b90253b37079eeebbf203d
Sajidh Yazeen <155213605+Agentz47@users.noreply.github.com>
2026-06-20 16:03:53 +0530

    Lombok added to StudentResponse model. no more manual getters needed 😁

--- src/main/java/com/sajidh/dto/StudentResponseDTO.java
+++ src/main/java/com/sajidh/dto/StudentResponseDTO.java
@@ -1,35 +1,16 @@
 package com.sajidh.dto;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class StudentResponseDTO {
 
     private int id;
     private String name;
     private int age;
     private String course;
-
     private String departmentName;
-
-    public StudentResponseDTO(int id, String name, int age, String course, String departmentName) {
-
-        this.id = id;
-        this.name = name;
-        this.age = age;
-        this.course = course;
-        this.departmentName = departmentName;
-    }
-
-    public int getId() {
-        return id;
-    }
-    public String getName() {
-        return name;
-    }
-    public int getAge() {
-        return age;
-    }
-    public String getCourse() {
-        return course;
-    }
-    public String getDepartmentName() {return departmentName;}
 }
---
+++ src/main/java/com/sajidh/model/TestLombok.java
@@ -0,0 +1,14 @@
+package com.sajidh.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class TestLombok {
+
+    private String name;
+
+}
